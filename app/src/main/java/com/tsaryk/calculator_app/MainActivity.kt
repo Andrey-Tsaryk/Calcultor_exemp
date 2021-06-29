2067a0ec68af96fb1ce5cb9d85c73f4f811ab886
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-06-29 18:38:24 +0300

    code start

--- app/src/main/java/com/tsaryk/calculator_app/MainActivity.kt
+++ app/src/main/java/com/tsaryk/calculator_app/MainActivity.kt
@@ -2,10 +2,83 @@ package com.tsaryk.calculator_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_clear.setOnClickListener {
+            input.text = ""
+            output.text = ""
+        }
+        button_bracket_left.setOnClickListener {
+            input.text = addToInputText("(")
+        }
+        button_bracket_right.setOnClickListener {
+            input.text = addToInputText(")")
+        }
+        button_0.setOnClickListener {
+            input.text = addToInputText("0")
+        }
+        button_1.setOnClickListener {
+            input.text = addToInputText("1")
+        }
+        button_2.setOnClickListener {
+            input.text = addToInputText("2")
+        }
+        button_3.setOnClickListener {
+            input.text = addToInputText("3")
+        }
+        button_4.setOnClickListener {
+            input.text = addToInputText("4")
+        }
+        button_5.setOnClickListener {
+            input.text = addToInputText("5")
+        }
+        button_6.setOnClickListener {
+            input.text = addToInputText("6")
+        }
+        button_7.setOnClickListener {
+            input.text = addToInputText("7")
+        }
+        button_8.setOnClickListener {
+            input.text = addToInputText("8")
+        }
+        button_9.setOnClickListener {
+            input.text = addToInputText("9")
+        }
+        button_dot.setOnClickListener {
+            input.text = addToInputText(".")
+        }
+        button_division.setOnClickListener {
+            input.text = addToInputText("/")
+        }
+        button_multiply.setOnClickListener {
+            input.text = addToInputText("*")
+        }
+        button_subtraction.setOnClickListener {
+            input.text = addToInputText("-")
+        }
+        button_addition.setOnClickListener {
+            input.text = addToInputText("+")
+        }
+
+        buttom_equals.setOnClickListener{
+            showResult()
+        }
+
+    }
+    private fun addToInputText(buttonValue: String): String{
+        return "${input.text}$buttonValue"
+    }
+    private fun getInputExpression(): String{
+        var expression = input.text.replace(Regex("/"),"/")
+    }
+    private fun showResult (){
+
     }
 }
